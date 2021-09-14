aa28226208707e4477eb2816b0e13719c04721af
Brett Wagner <wagnerb@udel.edu>
2021-09-14 18:40:06 -0400

    added login and welcome pages

--- src/HealthCheck.py
+++ src/HealthCheck.py
@@ -1,13 +1,29 @@
+import os
 from time import sleep
 
 from selenium import webdriver
 
 browser = webdriver.Chrome(executable_path="C:/Users/got2b/Documents/Random Code/HealthCheck/chromedriver.exe")
 browser.get('http://covidcheck.udel.edu/')
-sleep(2)
 
-#Click Student/Employee amd ->
+
+# Click Student/Employee amd ->
+sleep(1)
 studentEmployee = browser.find_element_by_xpath('//*[@id="QID32-4-label"]')
 studentEmployee.click()
-nextButton = browser.find_element_by_xpath('//*[@id="NextButton"]')
-nextButton.click()
+studentButton = browser.find_element_by_xpath('//*[@id="NextButton"]')
+studentButton.click()
+
+# Username and Password
+sleep(1)
+username = browser.find_element_by_xpath('//*[@id="udelnetid"]')
+username.send_keys(os.getenv("USERNAME"))
+password = browser.find_element_by_xpath('//*[@id="pword"]')
+password.send_keys(os.getenv('PASSWORD'))
+login = browser.find_element_by_xpath('/html/body/div[3]/div/div[2]/form/div/div/div[4]/div/button')
+login.click()
+
+# Welcome Page Next Click
+sleep(1)
+welcomeButton = browser.find_element_by_xpath('//*[@id="NextButton"]')
+welcomeButton.click()
