d7dc4bbb3327f91945e7c443f8da8c8b253e3c00
Brett Wagner <wagnerb@udel.edu>
2022-03-03 19:41:10 -0500

    Trying updated selenium

--- HealthCheck.py
+++ HealthCheck.py
@@ -1,58 +1,63 @@
 import os
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def HealthCheck():
+    service = Service(ChromeDriverManager.install())
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    browser = webdriver.Chrome('chromedriver', chrome_options=chrome_options)
+    browser = webdriver.Chrome(service=service, chrome_options=chrome_options)
 
     browser.get('http://covidcheck.udel.edu/')
 
 
     # Click Student/Employee amd ->
-    sleep(1)
-    studentEmployee = browser.find_element_by_xpath('//*[@id="QID32-4-label"]')
+    sleep(2)
+    studentEmployee = browser.find_element(By.XPATH, '//*[@id="QID32-4-label"]')
     studentEmployee.click()
-    studentButton = browser.find_element_by_xpath('//*[@id="NextButton"]')
+    studentButton = browser.find_element(By.XPATH, '//*[@id="NextButton"]')
     studentButton.click()
 
     # Username and Password
-    sleep(1)
-    username = browser.find_element_by_xpath('//*[@id="udelnetid"]')
+    sleep(2)
+    username = browser.find_element(By.XPATH, '//*[@id="udelnetid"]')
     username.send_keys(os.getenv("USERNAME"))
-    password = browser.find_element_by_xpath('//*[@id="pword"]')
+    password = browser.find_element(By.XPATH, '//*[@id="pword"]')
     password.send_keys(os.getenv('PASSWORD'))
-    login = browser.find_element_by_xpath('/html/body/div[3]/div/div[2]/form/div/div/div[4]/div/button')
+    login = browser.find_element(By.XPATH, '/html/body/div[3]/div/div[2]/form/div/div/div[4]/div/button')
     login.click()
 
     # Welcome Page Next Click
-    sleep(1)
-    welcomeButton = browser.find_element_by_xpath('//*[@id="NextButton"]')
+    sleep(10)
+    print("Entered Health Checker")  # TODO: Remove for master
+    welcomeButton = browser.find_element(By.XPATH, '//*[@id="NextButton"]')
     welcomeButton.click()
 
     # Skip Phone Number
     sleep(2)
-    phoneButton = browser.find_element_by_xpath('//*[@id="NextButton"]')
+    phoneButton = browser.find_element(By.XPATH, '//*[@id="NextButton"]')
     phoneButton.click()
 
     # Past 2 Weeks
     sleep(2)
-    none2Weeks = browser.find_element_by_xpath('//*[@id="QID4-5-label"]')
+    none2Weeks = browser.find_element(By.XPATH, '//*[@id="QID4-5-label"]')
     none2Weeks.click()
-    sleep(1)
-    weeksButton = browser.find_element_by_xpath('//*[@id="NextButton"]')
+    sleep(2)
+    weeksButton = browser.find_element(By.XPATH, '//*[@id="NextButton"]')
     weeksButton.click()
 
     # 24 Hours
     sleep(2)
-    none24 = browser.find_element_by_xpath('//*[@id="QID6-5-label"]/span')
+    none24 = browser.find_element(By.XPATH, '//*[@id="QID6-5-label"]/span')
     none24.click()
-    hoursButton = browser.find_element_by_xpath('//*[@id="NextButton"]')
+    hoursButton = browser.find_element(By.XPATH, '//*[@id="NextButton"]')
     hoursButton.click()
     sleep(2)
 
